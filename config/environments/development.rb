0563c3570fa697a8b7bcde8fd5c1d3888eeffe80
Tom <tomrhernandez@gmail.com>
2015-08-11 04:10:05 +0000

    set up default boot and home pages route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  
+  # Disables console error message in server window
+  config.web_console.whiny_requests = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
